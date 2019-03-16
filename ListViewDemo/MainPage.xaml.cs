4ecc87af306fa9131843ca7e560242bcaadf9162
Dara Oladapo <daraoladapo@outlook.com>
2019-03-16 19:56:33 +0100

    ++headere

---
+++ ListViewDemo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListViewDemo.Models;
+using Xamarin.Forms;
+
+namespace ListViewDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            List<Student> Students = new List<Student>
+            {
+                new Student{FirstName="Dara", LastName="Oladapo", Level=500},
+                new Student{FirstName="Dayo", LastName="Ojo", Level=100}
+            };
+            StudentsListView.ItemsSource = Students;
+        }
+    }
+}
---
+++ ListViewDemo/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ListViewDemo.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get {return  $"{FirstName} {LastName}"; } }
+        public int Level { get; set; } = 100;//default level to 100
+    }
+}
